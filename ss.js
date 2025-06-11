433f281bec6311190c0d36822a2f3490cec31d4a
utblackbhai <utblackbhai@gmail.com>
2025-06-11 17:48:00 +0600

    Create ss.js

---
+++ ss.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+   // সব কীবোর্ড ইভেন্ট ব্লক করবে
+  document.addEventListener('keydown', function(e) {
+    e.preventDefault();
+  });
+
+  document.addEventListener('keypress', function(e) {
+    e.preventDefault();
+  });
+
+  document.addEventListener('keyup', function(e) {
+    e.preventDefault();
+  });
+</script>
